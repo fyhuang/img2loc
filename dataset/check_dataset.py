85d25fdba7edb6b1819d9456ce1608207b404270
Frank Huang <me@fyhuang.com>
2024-03-01 16:51:38 -0800

    Tiny CA-only dataset and tiny binary classifier

---
+++ dataset/check_dataset.py
@@ -0,0 +1,47 @@
+import sys
+import subprocess
+from pathlib import Path
+
+def print_context(filenames, i):
+    # Print the lines around the current index
+    print("Context:")
+    for j in range(i-2, i+3):
+        if j < 0 or j >= len(filenames):
+            continue
+        print(j, filenames[j])
+
+
+def check_filename_list(filenames):
+    all_good = True
+
+    # Iterate every two lines
+    for i in range(0, len(filenames), 2):
+        line1 = filenames[i]
+        line2 = filenames[i+1]
+
+        # Convert both to path
+        path1 = Path(line1)
+        path2 = Path(line2)
+
+        # Check that stems match
+        if path1.stem != path2.stem:
+            print("Stems do not match: ", path1, path2)
+            print_context(filenames, i)
+            all_good = False
+
+    return all_good
+
+
+def main():
+    # Get listing from tar file
+    filename = sys.argv[1]
+
+    result = subprocess.run(["tar", "-tf", filename], check=True, capture_output=True)
+    lines = result.stdout.splitlines()
+    filenames = [l.decode('utf-8') for l in lines]
+    if check_filename_list(filenames):
+        print("All OK")
+
+
+if __name__ == "__main__":
+    main()
